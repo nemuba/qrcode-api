268b0853d711ceba4953c5d57fd3dbc7a1a7a3b6
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-03-23 10:33:29 -0300

    fix errors

--- prisma/migrations/20210323121809_init_database/migration.sql
+++ prisma/migrations/20210323121809_init_database/migration.sql
@@ -4,9 +4,6 @@
   - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropTable
-DROP TABLE `users`;
-
 -- CreateTable
 CREATE TABLE `address` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
